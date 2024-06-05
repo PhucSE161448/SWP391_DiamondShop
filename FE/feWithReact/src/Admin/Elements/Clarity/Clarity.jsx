96a91b3f3fea3a88e84d4ea1e7458e1b1cba4a57
PhamVietHoangFPT <hoangpvse183123@fpt.edu.vn>
2024-06-06 00:15:03 +0700

    CRUD Clarity

--- FE/feWithReact/src/Admin/Elements/Clarity/Clarity.jsx
+++ FE/feWithReact/src/Admin/Elements/Clarity/Clarity.jsx
@@ -6,21 +6,20 @@ import UpdateClarity from './UpdateClarity';
 import DelelteClarity from './DelelteClarity';
 
 export default function Clarity() {
-    return (
-        <div className='contentAdminContainer'>
-            <div className='CRUDContainer '>
-                <div className='titleOfFormContainer'>
-                    <h2>Clarity</h2>
-                </div>
-                <div className='buttonContainer'>
-                    <CreateClarity></CreateClarity>
-                    <ReadClarity></ReadClarity>
-                    <ReadClarityByID ></ReadClarityByID>
-                    <UpdateClarity></UpdateClarity>
-                    <DelelteClarity></DelelteClarity>
-                </div>
-            </div>
-
-        </div>
-    )
+	return (
+		<div className='contentAdminContainer'>
+			<div className='CRUDContainer '>
+				<div className='titleOfFormContainer'>
+					<h2>Clarity</h2>
+				</div>
+				<div className='buttonContainer'>
+					<CreateClarity></CreateClarity>
+					<ReadClarity></ReadClarity>
+					<ReadClarityByID ></ReadClarityByID>
+					<UpdateClarity></UpdateClarity>
+					<DelelteClarity></DelelteClarity>
+				</div>
+			</div>
+		</div>
+	)
 }
--- FE/feWithReact/src/Admin/Elements/Clarity/UpdateClarity.jsx
+++ FE/feWithReact/src/Admin/Elements/Clarity/UpdateClarity.jsx
@@ -1,86 +1,85 @@
 import React, { useState } from 'react'
 
 export default function UpdateClarity() {
-    const [idClarity, setIdClarity] = useState('')
-    const [nameClarity, setnameClarity] = useState('')
-    const [colorClarity, setColorClarity] = useState('')
-    const [priceClarity, setpriceClarity] = useState('')
-    const [showUpdateCW, setShowUpdateCW] = useState(false)
-    const [context, setContext] = useState('UPDATE')
-    const [dataClarity, setDataClarity] = useState(null)
-    const handleClick = () => {
-        setShowUpdateCW(!showUpdateCW)
-        setContext(prevContext => prevContext === 'UPDATE' ? 'Click again to close UPDATE' : 'UPDATE')
-    }
+  const [idClarity, setIdClarity] = useState('')
+  const [nameClarity, setnameClarity] = useState('')
+  const [colorClarity, setColorClarity] = useState('')
+  const [priceClarity, setpriceClarity] = useState('')
+  const [showUpdateCW, setShowUpdateCW] = useState(false)
+  const [context, setContext] = useState('UPDATE')
+  const [dataClarity, setDataClarity] = useState(null)
+  const handleClick = () => {
+    setShowUpdateCW(!showUpdateCW)
+    setContext(prevContext => prevContext === 'UPDATE' ? 'Click again to close UPDATE' : 'UPDATE')
+  }
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        // Gọi hàm CreateCaratWeight, truyền weight và price như là các đối số
-        UpdateClarity(idClarity, nameClarity, colorClarity, priceClarity)
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // Gọi hàm CreateCaratWeight, truyền weight và price như là các đối số
+    UpdateClarity(idClarity, nameClarity, colorClarity, priceClarity)
+  }
 
-    const handleClear = () => {
-        setIdClarity('')
-        setnameClarity('')
-        setColorClarity('')
-        setpriceClarity('')
-    }
+  const handleClear = () => {
+    setIdClarity('')
+    setnameClarity('')
+    setColorClarity('')
+    setpriceClarity('')
+  }
 
-    function UpdateClarity(Id, Name, Color, Price) {
-        const url = 'https://localhost:7122/api/Clarity/Update/' + Id
-        const formData = new FormData()
-        formData.append('Id', Id)
-        formData.append('Name', Name)
-        formData.append('Color', Color)
-        formData.append('Price', Price)
-        fetch(url, {
-            method: 'PUT',
-            headers: {
-                'Accept': '*/*'
-            },
-            body: formData
-        })
-            .then(response => response.json())
-            .then(responseData => {
-                setDataClarity(responseData)
-            })
-    }
-    return (
-        <>
-            <button className='CRUDButton btn btn-warning btn-lg' onClick={handleClick}>{context}</button>
-            {showUpdateCW &&
-                <div className='formCRUDContainer'>
-                    <h3 className='titleOfForm'>UPDATE CLARITY</h3>
-                    <form onSubmit={handleSubmit} className='row'>
-                        <div className='col-3'>
-                            <input type="text" value={idClarity} onChange={e => setIdClarity(e.target.value)} className='form-control' placeholder='Id' />
-                        </div>
-                        <div className='col-3'>
-                            <input type="text" value={nameClarity} onChange={e => setnameClarity(e.target.value)} className='form-control' placeholder='Name' />
-                        </div>
-                        <div className='col-3'>
-                            <input type="text" value={colorClarity} onChange={e => setColorClarity(e.target.value)} className='form-control' placeholder='Color' />
-                        </div>
-                        <div className='col-3'>
-                            <input type="text" value={priceClarity} onChange={e => setpriceClarity(e.target.value)} className='form-control' placeholder='Price' />
-                        </div>
-                        {
-                            dataClarity ? (dataClarity.StatusCode === 404 ? (<h3>Clarity doesnt exist</h3>) : (
-                                dataClarity.status === 400 ? (
-                                    <h3>
-                                        {dataClarity.errors.Price}
-                                    </h3>
-                                ) : (<h3>Update successful</h3>))
-
-                            ) : null
-                        }
-                        <div className='formSubmit'>
-                            <input type="submit" value="Submit" className='btn btn-primary btn-lg submitButton' />
-                            <input type="button" value="Clear" onClick={handleClear} className='btn btn-danger btn-lg submitButton' />
-                        </div>
-                    </form>
-                </div>
+  function UpdateClarity(Id, Name, Color, Price) {
+    const url = 'https://localhost:7122/api/Clarity/Update/' + Id
+    const formData = new FormData()
+    formData.append('Id', Id)
+    formData.append('Name', Name)
+    formData.append('Color', Color)
+    formData.append('Price', Price)
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Accept': '*/*'
+      },
+      body: formData
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        setDataClarity(responseData)
+      })
+  }
+  return (
+    <>
+      <button className='CRUDButton btn btn-warning btn-lg' onClick={handleClick}>{context}</button>
+      {showUpdateCW &&
+        <div className='formCRUDContainer'>
+          <h3 className='titleOfForm'>UPDATE CLARITY</h3>
+          <form onSubmit={handleSubmit} className='row'>
+            <div className='col-3'>
+              <input type="text" value={idClarity} onChange={e => setIdClarity(e.target.value)} className='form-control' placeholder='Id' />
+            </div>
+            <div className='col-3'>
+              <input type="text" value={nameClarity} onChange={e => setnameClarity(e.target.value)} className='form-control' placeholder='Name' />
+            </div>
+            <div className='col-3'>
+              <input type="text" value={colorClarity} onChange={e => setColorClarity(e.target.value)} className='form-control' placeholder='Color' />
+            </div>
+            <div className='col-3'>
+              <input type="text" value={priceClarity} onChange={e => setpriceClarity(e.target.value)} className='form-control' placeholder='Price' />
+            </div>
+            {
+              dataClarity ? (dataClarity.StatusCode === 404 ? (<h3>Clarity doesnt exist</h3>) : (
+                dataClarity.status === 400 ? (
+                  <h3>
+                    {dataClarity.errors.Price}
+                  </h3>
+                ) : (<h3>Update successful</h3>))
+              ) : null
             }
-        </>
-    )
+            <div className='formSubmit'>
+              <input type="submit" value="Submit" className='btn btn-primary btn-lg submitButton' />
+              <input type="button" value="Clear" onClick={handleClear} className='btn btn-danger btn-lg submitButton' />
+            </div>
+          </form>
+        </div>
+      }
+    </>
+  )
 }
