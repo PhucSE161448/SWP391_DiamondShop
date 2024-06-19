71d8dd6490e9fcd838f23cb6fdac6d3553c5a23b
Phúcc <112921620+PhucSE161448@users.noreply.github.com>
2024-06-19 20:11:48 +0700

    fix upload image deploy

--- BE/SWP391_DiamondShop/Application/Commons/AppConfiguration.cs
+++ BE/SWP391_DiamondShop/Application/Commons/AppConfiguration.cs
@@ -14,10 +14,25 @@
         public string UserName { get; set; }
         public string Password { get; set; }
     }
+    public class GoogleImage
+    {
+        public string type { get; set; }
+        public string project_id { get; set; }
+        public string private_key_id { get; set; }
+        public string private_key { get; set; }
+        public string client_email { get; set; }
+        public string client_id { get; set; }
+        public string auth_uri { get; set; }
+        public string token_uri { get; set; }
+        public string auth_provider_x509_cert_url { get; set; }
+        public string client_x509_cert_url { get; set; }
+        public string universe_domain { get; set; }
+    }
     public class AppConfiguration
     {
         public string DatabaseConnection { get; set; }
         public JWTSection JWTSection { get; set; }
         public EmailConfig EmailConfiguration { get; set; }
+        public GoogleImage GoogleImage { get; set; }
     }
 }
