abce4eadaad061b82eb1c153efed42aacb63e061
PhamVietHoangFPT <hoangpvse183123@fpt.edu.vn>
2024-07-03 01:27:36 +0700

    Create Profile.jsx

---
+++ FE/feWithReact/src/Pages/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { jwtDecode } from 'jwt-decode'
+export default function Profile() {
+  const userDetail = jwtDecode(localStorage.getItem('token'))
+  console.log(userDetail)
+  return (
+    <>
+      <div>Profile</div>
+      <div>
+        <div>Username: {userDetail.Username}</div>
+        <div>Email: {userDetail.Email}</div>
+        <div>Role: {userDetail.Role}</div>
+      </div>
+    </>
+  )
+}
