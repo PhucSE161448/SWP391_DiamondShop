7a33fda67b44bfae8f81eb85358e3dad36133367
Nguyễn Anh Duy <duynguyenbt21093@gmail.com>
2024-07-10 11:15:40 +0700

    add base voucher API

---
+++ BE/SWP391_DiamondShop/Application/Services/Vouchers/VoucherService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Interfaces.Vouchers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Vouchers
+{
+    public class VoucherService : IVoucherService
+    {
+    }
+}
---
+++ BE/SWP391_DiamondShop/Infrastructures/Repositories/Vouchers/VoucherRepository.cs
@@ -0,0 +1,13 @@
+﻿using Application.IRepositories.Vouchers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructures.Repositories.Vouchers
+{
+    public class VoucherRepository : IVoucherRepository
+    {
+    }
+}
