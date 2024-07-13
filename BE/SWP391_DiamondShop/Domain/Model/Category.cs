ab0a8dc86e8342108bc42a490e674c538fb3d520
Châu Thiên Ân <chauthienan01072002@gmail.com>
2024-07-13 10:39:03 +0700

    Fix code

--- BE/SWP391_DiamondShop/Domain/Model/Category.cs
+++ BE/SWP391_DiamondShop/Domain/Model/Category.cs
@@ -12,9 +12,9 @@ namespace Domain.Model
 
         public int Id { get; set; }
         public string Name { get; set; } = null!;
-        public int GroupId { get; set; }
+        public int? GroupId { get; set; }
 
-        public virtual Group Group { get; set; } = null!;
+        public virtual Group? Group { get; set; } = null!;
         public virtual ICollection<Product> Products { get; set; }
     }
 }
