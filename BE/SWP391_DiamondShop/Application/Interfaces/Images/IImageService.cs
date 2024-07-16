62de642d813c197db98b8ef1f02a75417b422c18
Châu Thiên Ân <chauthienan01072002@gmail.com>
2024-07-17 03:30:42 +0700

    Fix Update Image Product & Diamond

--- BE/SWP391_DiamondShop/Application/Interfaces/Images/IImageService.cs
+++ BE/SWP391_DiamondShop/Application/Interfaces/Images/IImageService.cs
@@ -7,6 +7,6 @@ public interface IImageService
 {
     Task UploadDiamondImages(List<IFormFile> imageFiles, int diamondId);
     Task UploadProductImages(List<IFormFile> imageFiles, int productId);
-
+    Task<IFormFile> DownloadImageFromUrl(string imageUrl, string fileName, string contentType);
     Task DeleteImages(IEnumerable<Image> images);
 }
